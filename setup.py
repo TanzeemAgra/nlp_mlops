eacaf492915c8c98b7452b661af659c74cbb3f3e
Tanzeem Agra <xerxez.in@gmail.com>
2022-12-06 15:49:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mlops for NLP',
+    author='Xerxez Solutions',
+    license='MIT',
+)
